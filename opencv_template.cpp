5d8e69cd1743009b99e0e6dcfa6a38b441ae7ad3
Alex(ia) Lee <alexia.lee@uqconnect.edu.au>
2015-02-05 16:13:58 -0800

    Template files to avoid typing. Because laziness.
    Some generic makefile templates
    Generic cpp template
    Generic cpp template with added opencv goodness.
    
    Script to customise templates on the fly is on its way. Maybe.

---
+++ opencv_template.cpp
@@ -0,0 +1,22 @@
+/**
+ * opencv_template.cpp
+ *
+ * Template for an opencv program (using opencv 3), 
+ * containing typical headers.
+ */
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <iostream>
+#include <string>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv) {
+  return 0;
+}
+
---
+++ std.cpp
@@ -0,0 +1,14 @@
+/**
+ * FILE: std.cpp
+ * AUTHOR: avadendas
+ * DESCRIPTION: The most vanilla cpp template. 
+ */
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+int main(int argc, char** argv) {
+
+  return 0;
+}
